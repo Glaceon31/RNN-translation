ff1357c7e872b91db16f86093cdfca1fca4e4d23
Glaceon31 <grit31@126.com>
2015-09-04 21:22:37 +0800

    still some bug

--- rnnmodel.py
+++ rnnmodel.py
@@ -7,7 +7,7 @@ from collections import OrderedDict
 
 class rnnmodel(object):
 
-	def __init__(self, rng, sv, tv, hd):
+	def __init__(self, rng, inp, sv, tv, hd, maxl):
 		'''
 		rng: numpy.random.RandomState
 		sv: source vocabulary size
@@ -21,41 +21,52 @@ class rnnmodel(object):
 			.astype(theano.config.floatX))
 		self.outw = theano.shared(0.2*numpy.random.uniform(-1,1,(hd,tv))\
 			.astype(theano.config.floatX))
-
-		self.params = [self.inw, self.recurrent, self.outw]
+		self.h0 = theano.shared(numpy.zeros(hd, dtype=theano.config.floatX))
 
 		def recurrence(x_t, h_tm1):
 			h_t = T.nnet.sigmoid(x_t+T.dot(h_tm1, self.recurrent))
 			s_t = T.nnet.softmax(T.dot(h_t, self.outw))
 			return [h_t, s_t]
+		
+		self.input = inp
+		x = [self.inw[inp[0]]]
+		h = [self.inw[inp[0]]]
+		self.p_y_given_x = [T.nnet.softmax(T.dot(h[0], self.outw))]
+		self.pred = [T.argmax(self.p_y_given_x[0])]
+		for i in xrange(1,maxl):
+			x.append(self.inw[inp[i]])
+			h.append(x[i]+T.dot(h[i-1], self.recurrent))
+			self.p_y_given_x.append(T.nnet.softmax(T.dot(h[i], self.outw)))
+			self.pred.append(T.argmax(self.p_y_given_x[i]))
+		#self.pred = theano.
 
-		inp = T.imatrix()
+		#x = theano.shared(numpy.asarray(x))
+		#x = self.inw[inp].reshape((inp.shape[0], hd*sl))
 
-		y = T.iscalar('y')
-		sl = T.iscalar('sl')
-		lr = T.scalar('lr')
+		#[h, s], _ = theano.scan(fn=recurrence, \
+		#	sequences=x, outputs_info=[self.h0, None], \
+		#	n_steps=x.shape[0])
 
-		x = self.inw[inp].reshape((inp.shape[0], hd*sl))
+		#self.p_y_given_x = s[-1,0,:]
 
-		[h, s], _ = theano.scan(fn=recurrence, \
-			sequences=x, outputs_info=[self.outw, None], \
-			n_steps=x.shape[0])
+		#p_y_given_x_lastword = s[-1,0,:]
+		#p_y_given_x_sentence = s[:,0,:]
+		#y_pred = T.argmax(p_y_given_x_sentence, axis=1)
 
-		p_y_given_x_lastword = s[-1,0,:]
-		p_y_given_x_sentence = s[:,0,:]
-		y_pred = T.argmax(p_y_given_x_sentence, axis=1)
+		#nll = -T.mean(T.log(p_y_given_x_lastword)[y])
+		#gradients = T.grad( nll, self.params )
+		#updates = OrderedDict(( p, p-lr*g ) for p, g in zip( self.params , gradients))
 
-		nll = -T.mean(T.log(p_y_given_x_lastword)[y])
-		gradients = T.grad( nll, self.params )
-		updates = OrderedDict(( p, p-lr*g ) for p, g in zip( self.params , gradients))
+		#self.prediction = theano.function(inputs = [inp, sl], outputs= y_pred)
+		#self.train = theano.function(inputs = [inp, y, sl], outputs = nll, \
+		#	updates = updates)
+		#self.normalize = theano.function( inputs = [],
+        #                 updates = {self.emb:\
+        #                 self.emb/T.sqrt((self.emb**2).sum(axis=1)).dimshuffle(0,'x')})
 
-		y_pred = T.argmax(p_y_given_x_sentence, axis=1)
-
-		self.prediction = theano.function(inputs = [inp, sl], outputs= y_pred)
-		self.train = theano.function(inputs = [inp, y, sl], outputs = nll, updates = updates)
-
-
-
+	def negative_log_likelihood(self, y):
+		return -T.mean(T.log(self.p_y_given_x)[T.arange(y.shape[0]), y])
+		#return -T.mean(T.log(self.p_y_given_x)[T.arange(y.shape[0]), y])
 
 
 
--- rnntest.py
+++ rnntest.py
@@ -8,8 +8,12 @@ import subprocess
 import os
 import random
 
+import theano
+from theano import tensor as T
+
 if __name__ == '__main__':
 
+	learning_rate = 0.1
 	train_set = [[['I', 'like', 'music', '</eos>'],['I', 'don\'t', 'like', 'painting', '</eos>']],[['我','喜欢','音乐', '</eos>'],['我','不','喜欢','画画', '</eos>']]]
 	valid_set = [[['I', 'don\'t', 'like', 'music', '</eos>']],[['我','不','喜欢','音乐', '</eos>']]]
 	test_set = [[['I', 'like', 'painting', '</eos>']],[['我','喜欢','画画', '</eos>']]]
@@ -21,9 +25,14 @@ if __name__ == '__main__':
 	valid_input, valid_output = valid_set
 	test_input, test_output = test_set
 
+	maxslength = 0
+	maxtlength = 0
+
 	#construct dictionary
 	for input_set in [train_input, valid_input, test_input]:
 		for sentence in input_set:
+			if len(sentence) > maxslength:
+				maxslength = len(sentence)
 			for i in range(0,len(sentence)):
 				word = sentence[i]
 				if not word in sourcedict:
@@ -32,26 +41,75 @@ if __name__ == '__main__':
 
 	for output_set in [train_output, valid_output, test_output]:
 		for sentence in output_set:
+			if len(sentence) > maxtlength:
+				maxtlength = len(sentence)
 			for i in range(0,len(sentence)):
 				word = sentence[i]
 				if not word in targetdict:
 					targetdict.append(word)
 				sentence[i] = targetdict.index(word)
 
+	print maxslength, maxtlength
+	for input_set in [train_input, valid_input, test_input]:
+		for i in xrange(len(input_set)):
+			sentence = input_set[i]
+			if len(sentence) < maxslength:
+				for i in xrange(maxslength-len(sentence)):
+					sentence.append(0)
+
+	for output_set in [train_output, valid_output, test_output]:
+		for i in xrange(len(output_set)):
+			sentence = output_set[i]
+			if len(sentence) < maxslength:
+				for i in xrange(maxslength-len(sentence)):
+					sentence.append(0)
+
+	nsentence = len(train_input)
+	svocsize = len(sourcedict)
+	tvocsize = len(targetdict)
 	print sourcedict
 	print targetdict
 	print train_input
 	print train_output
 
-	nsentence = len(train_input)
-	svocsize = len(sourcedict)
-	tvocsize = len(targetdict)
+	train_input = theano.shared(numpy.asarray(train_input))
+	train_output = theano.shared(numpy.asarray(train_output))
 
+	print train_input
+	print train_output
+	#construct model
+	
 	rng = numpy.random.RandomState(23455)
-	rnn = rnnmodel(rng, svocsize, tvocsize, 100)
+	index = T.lscalar()
 
-	for epoch in range(0, 200):
+	x = T.lvector('x')
+	y = T.lvector('y')
+	rnn = rnnmodel(rng, x, svocsize, tvocsize, 100, maxslength)
+
+	cost = rnn.negative_log_likelihood(y)
+	params = [rnn.inw, rnn.recurrent, rnn.outw]
+	grads = T.grad(cost, params)
+	updates = [
+        (param_i, param_i - learning_rate * grad_i)
+        for param_i, grad_i in zip(params, grads)
+    ]
+
+	train_model = theano.function(
+		[index],
+		cost,
+		updates=updates,
+		givens={x: train_input[index],
+				y: train_output[index]}
+	)
+
+	test_model = theano.function(
+		[train_input[0]],
+		rnn.pred
+		)
+
+	for epoch in range(0, 2):
 		print epoch
 		for i in range(0, nsentence):
-			rnn.train(train_input[i], train_output[i], len(train_input))
-		print rnn.predict(train_input[0])
+			cost = train_model(i)
+
+	print test_model()
